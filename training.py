c058aa6637548d38b7d6bcfe4cba58849829ba94
YoTi <yasserrafai5@gmail.com>
2022-11-14 04:53:23 +0100

    build the chatbot

--- training.py
+++ training.py
@@ -63,3 +63,19 @@ train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
 # Build a neural network
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y),
+          epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print(done)
